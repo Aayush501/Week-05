4b3bf9a3f70539c67a5d7cfa0125b26ef6148f39
Aayush501 <shrivastavashu501@gmail.com>
2025-02-18 14:58:58 +0530

    [Aayush Kumar Shrivastav] Add . Read a CSV File and Print Data -> (Solution + Test)

---
+++ Day01/untitled/src/main/csvdatahandling/basicproblems/ReadCSVFile.java
@@ -0,0 +1,41 @@
+package csvdatahandling.basicproblems;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadCSVFile {
+    // created a method to read and get the data from csv file
+    public static ArrayList<ArrayList<String >> readCsvFile(String file) throws CsvValidationException, IOException {
+        CSVReader reader = new CSVReader(new FileReader(file));
+        // 2d string arraylist to store the data
+        ArrayList<ArrayList<String >> csvData = new ArrayList<>();
+        String[] nextLine; // to store read row
+        while((nextLine = reader.readNext()) != null) { // reading the file
+            ArrayList<String> line = new ArrayList<>(List.of(nextLine));
+            csvData.add(line);
+        }
+        return csvData; // returning csv data
+    }
+
+    public static void main(String[] args) {
+        String file = "readcsv.csv"; // file path
+
+        // try with resources to close the resources after reading the file
+        try { // using openCSV library to read the CSV file
+            ArrayList<ArrayList<String >> csvData; // 2D string arrayList to store the file data
+            csvData = readCsvFile(file);
+            for (ArrayList<String> strings : csvData) {
+                System.out.println(strings);
+            }
+        }
+        // handling the exception
+        catch (Exception e) {
+            System.out.println("Exception Occurred");
+        }
+    }
+}
---
+++ Day01/untitled/src/test/csvdatahandling/basicproblems/ReadCSVFileTest.java
@@ -0,0 +1,31 @@
+package csvdatahandling.basicproblems;
+import com.opencsv.exceptions.CsvValidationException;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReadCSVFileTest {
+    // created a method to test the readCsvFile method
+    @Test
+    public void readCSVFile() throws CsvValidationException, IOException {
+        // paths of test files
+        String test1 = "csvTest1.csv", test2 = "csvTest2.csv", test3 = "csvTest3.csv";
+
+        // defining the expected result arrays
+        ArrayList<ArrayList<String>> result1 = new ArrayList<>();
+        result1.add(new ArrayList<String >(Arrays.asList("Aayush","Kumar","Shrivastav","Java","Programming","Learner")));
+        ArrayList<ArrayList<String>> result2 = new ArrayList<>();
+        result2.add(new ArrayList<String >(Arrays.asList("technocrats","institute","of","technology","excellence","bhopal")));
+        ArrayList<ArrayList<String>> result3 = new ArrayList<>();
+        result3.add(new ArrayList<String >(Arrays.asList("bridge","labz","teaching","us","java")));
+
+        // performing tests
+        assertEquals(result1, ReadCSVFile.readCsvFile(test1));
+        assertEquals(result2, ReadCSVFile.readCsvFile(test2));
+        assertEquals(result3, ReadCSVFile.readCsvFile(test3));
+    }
+}
